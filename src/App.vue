bccbc93187ce00f879aef820676967188558b6c6
Idaika Iglesias <idaik.ocio@gmail.com>
2020-02-19 20:26:43 +0100

    añadido componente calendario

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calendar></Calendar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from 'v-year-calendar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calendar
   }
 }
 </script>
