7e89f801700ca65eda223dedac75b4d9baaff700
Tako Schotanus <tako@codejive.org>
2020-03-03 16:28:40 +0000

    Make dev server works in gitpod

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.REPOSITORY_NAME + '/'
-    : '/'
+    : '/',
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true
+  }
 }
-
